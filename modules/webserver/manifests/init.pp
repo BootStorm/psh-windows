ce3d376eecb8817a7f5eb5d30e132f3a1923a56e
James Forrest <james@forrestclan.com>
2015-10-19 02:33:38 +0000

    added a basic website

--- modules/webserver/manifests/init.pp
+++ modules/webserver/manifests/init.pp
@@ -7,5 +7,17 @@ class webserver {
     ensure => present,
   }
 
+  iis::manage_app_pool { 'my_first_pool':
+    enable_32_bit           => true,
+    managed_runtime_version => 'v4.0',
+  }
+
+  iis::manage_site {'www.testsite.com':
+    site_path   => 'C:\inetpub\wwwroot\testsite',
+    port        => '80',
+    ip_address  => '*',
+    host_header => 'www.testsite.com',
+    app_pool    => 'my_first_pool',
+  }
 }
 
